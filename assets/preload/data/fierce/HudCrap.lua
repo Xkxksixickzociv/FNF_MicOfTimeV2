4b747b7d9fb7f537f707328b39bb8bdb8f71208d
Xkxksixickzociv <119426594+Xkxksixickzociv@users.noreply.github.com>
2023-01-15 00:13:38 +0200

    Add files via upload

---
+++ assets/preload/data/fierce/HudCrap.lua
@@ -0,0 +1,30 @@
+function onUpdate(elapsed) -- STOP LOOKING AROUND
+    if keyPressed('left') then
+        doTweenColor('timeBar', 'timeBar', '808080', 0.01, 'linear');
+    elseif keyPressed('right') then
+        doTweenColor('timeBar', 'timeBar', '808080', 0.01, 'linear');
+    elseif keyPressed('down') then
+        doTweenColor('timeBar', 'timeBar', '808080', 0.01, 'linear');
+    elseif keyPressed('up') then
+        doTweenColor('timeBar', 'timeBar', '808080', 0.01, 'linear');
+    else
+        doTweenColor('timeBar', 'timeBar', '808080', 0.4, 'linear');
+    end
+end
+
+local angleshit = -1
+
+function onBeatHit() --bruh
+	if getProperty('curBeat') % 2 == 0 then
+			setProperty('iconP1.angle',angleshit*20)
+			setProperty('iconP2.angle',angleshit*0)
+			doTweenAngle('hr', 'iconP1', 0, 0.5, 'circOut')
+			doTweenAngle('hrr', 'iconP2', 0, 0.5, 'circOut')
+	    end
+	if getProperty('curBeat') % 2 == 0 then
+			setProperty('iconP1.angle',angleshit*20)
+			setProperty('iconP2.angle',angleshit*-20)
+			doTweenAngle('hr', 'iconP1', 0, 0.5, 'circOut')
+			doTweenAngle('hrr', 'iconP2', 0, 0.5, 'circOut')
+		end
+end
