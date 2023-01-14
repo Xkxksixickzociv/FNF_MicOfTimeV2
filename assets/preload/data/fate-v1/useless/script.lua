fdf49421cea1441050beee57e248b2cccf9c8d31
Xkxksixickzociv <119426594+Xkxksixickzociv@users.noreply.github.com>
2023-01-15 00:12:06 +0200

    Add files via upload

---
+++ assets/preload/data/fate-v1/useless/script.lua
@@ -0,0 +1,69 @@
+local followchars = true;
+local del = 0;
+local del2 = 0;
+local xx = 2000;
+local yy = 1200;
+local xx2 = 2800;
+local yy2 = 1300;
+local ofs = 50;
+function onUpdate()
+	if followchars == true then
+        if mustHitSection == false then
+            if getProperty('dad.animation.curAnim.name') == 'singLEFT' then
+                triggerEvent('Camera Follow Pos',xx-ofs,yy)
+            end
+            if getProperty('dad.animation.curAnim.name') == 'singRIGHT' then
+                triggerEvent('Camera Follow Pos',xx+ofs,yy)
+            end
+            if getProperty('dad.animation.curAnim.name') == 'singUP' then
+                triggerEvent('Camera Follow Pos',xx,yy-ofs)
+            end
+            if getProperty('dad.animation.curAnim.name') == 'singDOWN' then
+                triggerEvent('Camera Follow Pos',xx,yy+ofs)
+            end
+            if getProperty('dad.animation.curAnim.name') == 'idle' then
+                triggerEvent('Camera Follow Pos',xx,yy)
+            end
+            if getProperty('dad.animation.curAnim.name') == 'Laugh' then
+                triggerEvent('Camera Follow Pos',xx,yy)
+            end
+        else
+
+            if getProperty('boyfriend.animation.curAnim.name') == 'singLEFT' then
+                triggerEvent('Camera Follow Pos',xx2-ofs,yy2)
+            end
+            if getProperty('boyfriend.animation.curAnim.name') == 'singRIGHT' then
+                triggerEvent('Camera Follow Pos',xx2+ofs,yy2)
+            end
+            if getProperty('boyfriend.animation.curAnim.name') == 'singUP' then
+                triggerEvent('Camera Follow Pos',xx2,yy2-ofs)
+            end
+            if getProperty('boyfriend.animation.curAnim.name') == 'singDOWN' then
+                triggerEvent('Camera Follow Pos',xx2,yy2+ofs)
+            end
+            if getProperty('dad.animation.curAnim.name') == 'idle-alt' then
+                triggerEvent('Camera Follow Pos',xx2,yy2)
+            end
+            if getProperty('boyfriend.animation.curAnim.name') == 'idle' then
+                triggerEvent('Camera Follow Pos',xx2,yy2)
+            end
+        end
+    else
+        triggerEvent('Camera Follow Pos','','')
+    end	
+	if curStep == 672 then
+		doTweenZoom('cool', 'camGame', 0.8, 1, 'linear')
+	end
+
+	if curStep == 704 then
+		doTweenZoom('cool', 'camGame', 0.8, 0.3, 'linear')
+	end
+
+	if curStep == 1919 then
+		setScore(0);
+		setHits(0);
+		setRatingPercent(0);
+		setMisses(0)
+	end		
+	setCharacterX('dad',1700)
+end
