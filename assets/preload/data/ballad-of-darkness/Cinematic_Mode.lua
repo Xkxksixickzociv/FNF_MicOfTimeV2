897f9b7c9d9832cc87012a2791cb6d51a27ec642
Xkxksixickzociv <119426594+Xkxksixickzociv@users.noreply.github.com>
2023-01-15 00:01:22 +0200

    Add files via upload

---
+++ assets/preload/data/ballad-of-darkness/Cinematic_Mode.lua
@@ -0,0 +1,9 @@
+function onCreatePost()
+	setProperty('timeBar.visible', true)
+	setProperty('timeBarBG.visible', true)
+	setProperty('timeTxt.visible', true)
+	setProperty('healthBar.visible', false)
+	setProperty('healthBarBG.visible', false)
+	setProperty('iconP1.visible', false)
+	setProperty('iconP2.visible', false)
+	end
